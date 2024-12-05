e4b4cb811b4771c952b84a052a4d394175a2a67a
wary <evenfbh@gmail.com>
2024-12-05 12:48:25 +0100

    Redefined settings

--- videoworld/users/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-class ProfileModel(models.Model):
-    user_id = models.OneToOneField(User, on_delete=models.CASCADE)
-    user_desc = models.CharField(max_length=40, null=True, blank=True)
-    def __str__(self, user_id):
-        return self.user_id.username
--- videoworld/videos/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-class VideoModel(models.Model):
-    video_uploader = models.ForeinKey(User, on_delete=models.CASCADE)
-    video_title = models.CharField(max_length=70, null=False)
-    video_src = models.FileField(upload_to='videos_uploaded', null=False)
-    video_date = models.DateField()
