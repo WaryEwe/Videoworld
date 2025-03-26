1dcfd8abcf4654b84ee9e8b05e622576813de8a3
Wary <evenfbh@gmail.com>
2025-03-26 14:06:11 +0100

    Added extra security by hiding secret key and database credentials in a local file

--- videoworld/settings.py
+++ videoworld/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-oma$e#jax$wndh3r-u6s!4y*on6-t=!!r#+)%l=cx#ov=jtxok'
 
+SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -82,10 +83,10 @@ WSGI_APPLICATION = 'videoworld.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'USER':'videoworld',
-        'PASSWORD': 'password',
-        'NAME': 'videoworlddb',
-        'HOST': 'localhost',
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'NAME': config('DB_NAME'),
+        'HOST': config('DB_HOST'),
     }
 }
 
